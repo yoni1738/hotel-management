f643ffde17efb278aecf287a71f4eed040146c62
Bereket-kibret <139618642+Bereket-kibret@users.noreply.github.com>
2023-09-14 15:28:06 +0300

    Innitiating

--- 01.cpp
+++ 01.cpp
@@ -1,6 +1,38 @@
-#include <iostream>;
+#include<iostream>
 using namespace std;
-// function declaration
+class custemer{
+public:
+  int from_date;
+  int to_date;
+  string name;
+  int age;
+  char sex;
+  int idno;
+  string address;
+  int phone;
+};
+class room{
+public:
+  string type;
+  int roomno;
+  custemer cust;
+  int day;
+  float price;
+  int status=0;
+
+};
+class hotelmng{
+private:
+  int singleR=6;
+  int doubleR=6;
+  int familyR=6;
+  room rm[18];
+  int roomcount;
+public:
+  void check_in();
+  void check_out();
+  void showRoomInfo();
+  };
 int main(){
- cout<<"hello world";
+
 }
