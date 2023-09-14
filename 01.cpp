2fb5a31dc67b4bb07d2bc8c38ad273541c8535b2
Yonas-B <yonasbirhanu1738@gmail.com>
2023-09-14 16:05:17 +0300

    using a file system

--- 01.cpp
+++ 01.cpp
@@ -40,6 +40,53 @@ public:
   void check_in();
   void check_out();
   void showRoomInfo();
+void getstatus(){
+		int roomnum,i;
+		ifstream infile("rooms.txt", ios::in);
+		infile>>singleR>>doubleR>>familyR;
+		infile.close();
+		ifstream in("bookedstatus.txt", ios::in);
+		while(in>>roomnum){
+
+			i = roomnum - 1;
+			rm[i].roomno=roomnum;
+			rm[i].status=1;
+			in>>rm[i].type;
+			in>>rm[i].cust.name;
+			in>>rm[i].cust.idno;
+			in>>rm[i].cust.age;
+			in>>rm[i].cust.sex;
+			in>>rm[i].cust.address;
+			in>>rm[i].cust.phone;
+			in>>rm[i].cust.from_date;
+			in>>rm[i].cust.to_date;
+			rm[i].day = rm[i].cust.to_date - rm[i].cust.from_date;
+		}
+		in.close();
+	}
+	void save(){
+		ofstream outfile("rooms.txt", ios::out);
+		outfile<<singleR<<"\t"<<doubleR<<"\t"<<familyR;
+		outfile.close();
+
+		ofstream out;
+		out.open("bookedstatus.txt",ios::out);
+		for(int i=0;i<18;i++){
+			if(rm[i].status != 0){
+				out<<rm[i].roomno;
+				out<<"\t"<<rm[i].type;
+				out<<"\t"<<rm[i].cust.name;
+				out<<"\t"<<rm[i].cust.idno;
+				out<<"\t"<<rm[i].cust.age;
+				out<<"\t"<<rm[i].cust.sex;
+				out<<"\t"<<rm[i].cust.address;
+				out<<"\t"<<rm[i].cust.phone;
+				out<<"\t"<<rm[i].cust.from_date;
+				out<<"\t"<<rm[i].cust.to_date<<endl;
+			}
+		}
+		out.close();
+	}
   };
 int main(){
 
